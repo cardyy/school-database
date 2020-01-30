b01cecb753e8033dd67888cd82493c99a51b21b9
cardinal <cardyy@gmail.com>
2020-01-30 20:49:57 +0200

    make it better

--- index.js
+++ index.js
@@ -189,9 +189,9 @@ app.post('/users',function (req,res){
  var usernameIsPresent = data[0].students.some(function(el){ return el.email === username})
   var passwordIsPresent = data[0].students.some(function(el){ return el.password === password})
 if (usernameIsPresent === true && passwordIsPresent === true){
-  res.send({success:true, user:username}) ;
+  res.send({'success':true, 'user':username}) ;
   } else {
-   res.send({success:false , message:"No such user in our database!"}) ;
+   res.send({'success':false , 'message':"No such user in our database!"}) ;
   }
     });});
     
