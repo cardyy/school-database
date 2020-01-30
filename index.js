c619a82d0ba1582f783b2fcc6ccd6668c803c324
cardinal <cardyy@gmail.com>
2020-01-30 16:24:18 +0200

    make it better

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.post('/users',function (req,res){
  records.find({}, function (err,data){
   if (err) throw err;
   const std = data[0].students.find( ({ firstName }) => firstName === "Sean")
-   res.send({message:"Herroooo"}) ;
+   res.send({message:req.body.username}) ;
     });});
     
 
