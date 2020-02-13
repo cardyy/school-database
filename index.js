364795478dd1a0532ac98918df52822c481fbeee
cardinal <cardyy@gmail.com>
2020-02-13 15:18:22 +0200

    make it better

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ app.post('/users',function (req,res){
  app.get('/users',function (req,res){
  records.find({}, function (err,data){
  	if (err) throw err;
-   res.send({'username':"blams"}) ;
+   res.send([{'username':"blams"},{'username':"max"},{'username':"ralpgh"}]) ;
     });});
  
   
