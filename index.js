248babf80276516acb659f63e526518a2b250914
cardinal <cardyy@gmail.com>
2020-02-14 11:04:39 +0200

    make it better

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.post('/users',function (req,res){
   const password = req.body.password
    records.find({}, function (err,data){
     if (err) throw err;
-    for var(i in data){ const usernameIsPresent = data[i].students.some(function(el){ return el.email === username && el.password === password})}
+    for (i=0;i<data.length;i++){ const usernameIsPresent = data[i].students.some(function(el){ return el.email === username && el.password === password})}
       if (usernameIsPresent === true ){
       	result = data.filter(a => a.students.some(u => u.email==username && u.password==password));
       	const schoolId = result[0]._id
