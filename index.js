d86e098b06d829409f437c8a54489f4403af179b
cardinal <cardyy@gmail.com>
2020-02-22 15:16:58 +0200

    make it better

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ app.post('/users',function (req,res){
        payment.add("Bananas", 2.5);
         paynow.sendMobile(
          payment, 
-          '0774444444',
+          '0773333333',
            'ecocash' 
             ).then(function(response) {
              if(response.success) {
