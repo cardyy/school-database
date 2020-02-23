26ae4c75ac8ba7196a5d11b5176fe4716fb4fb40
cardinal <cardyy@gmail.com>
2020-02-23 06:19:06 +0200

    make it better

--- index.js
+++ index.js
@@ -213,8 +213,8 @@ app.post('/users',function (req,res){
       let payment = paynow.createPayment("Invoice 37", "cardyy@gmail.com");	
        const item = req.body.item
         const amount = req.body.amount
-        const ecoNumber = req.body.ecoNumber
-        console.log('wawa is', ecoNumber)
+        let phoneNumber = req.body.phoneNumber
+        console.log('wawa is', phoneNumber)
        payment.add(item, amount);
         paynow.sendMobile(
          payment, 
