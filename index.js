7ee0208379bfc045500b93b0c13998cb3825368a
Cardinal <cardyy@gmail.com>
2020-05-13 22:31:32 +0200

    make it better

--- index.js
+++ index.js
@@ -223,8 +223,10 @@ app.post('/users', function  (req,res){
       	    
       	     const schoolId = result[0]._id
       	     const stream = result[0].students.filter((s)=>s.email==username && s.password==password)[0].stream
+      	     const checkList = result[0].checkList.filter((s)=>s.stream==stream)
+
       	    
-	          res.send({'success':true, 'user':username, 'zita':schoolId,'stream':stream}) ;
+	          res.send({'success':true, 'user':username, 'zita':schoolId,'checkList':checkList}) ;
                } else {
                
                 res.send({'success':false , 'message':"No such user in our database!"}) ;
