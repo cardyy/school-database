e94e09a99e29d20ba3b936fc6bfdfd977fbd681e
Cardinal <cardyy@gmail.com>
2020-05-17 19:05:06 +0200

    make it better

--- index.js
+++ index.js
@@ -341,6 +341,7 @@ try {
     app.post('/store', async function (req,res){
     	let outletsArray
     	 outletsArray = await outlets.findById(req.body.outletId)
+          outletsArray.purchases= outletsArray.purchases.concat({school:'test'})
           let paynow = new Paynow("9130", "79e60b36-e2ee-48da-b2f4-a09ed08049d9");
            let payment = paynow.createPayment("Invoice 37", "cardyy@gmail.com");	
             const item = req.body.item
@@ -358,7 +359,16 @@ try {
                         console.log(instructions)
                 
                 //save to outlets
-            
+    
+try{
+ await outletsArray.save(function(err,data){
+	 if (err) throw err;
+	 	})
+ console.log('done')
+   }catch {
+	if(outletsArray== null){
+	 console.log('not done')}
+      }
                 
                          } else {
                          	       
