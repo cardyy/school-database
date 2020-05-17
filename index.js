dbadc12ebb821a41d72a57ec37d04f91c42e0a04
Cardinal <cardyy@gmail.com>
2020-05-17 14:33:55 +0200

    make it better

--- index.js
+++ index.js
@@ -339,6 +339,8 @@ try {
     });});
     
     app.post('/store',function (req,res){
+    	outletsArray = await outlets.findById(req.params.outletId)
+    	console.log(outletsArray)
      let paynow = new Paynow("9130", "79e60b36-e2ee-48da-b2f4-a09ed08049d9");
       let payment = paynow.createPayment("Invoice 37", "cardyy@gmail.com");	
        const item = req.body.item
@@ -354,6 +356,10 @@ try {
               let instructions = response.instructions 
                let pollUrl = response.pollUrl; 
                 console.log(instructions)
+                
+                //save to outlets
+                
+                
                          } else {
                           console.log(response.error) }
                            }).catch(ex => {
